09b23311725c1f1dcade0f74ee90d8163f713e0a
ariq <ghazi.ariq18@gmail.com>
2026-08-31 14:54:08 +0700

    update

--- todoweb/routes/users.js
+++ todoweb/routes/users.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+const moment = require('moment')
+const path = require('path');
 
 module.exports = function(db){
 
@@ -39,9 +41,9 @@ router.get('/', (req, res) => {
     
   }
 
-  if (req.query.isMarried) {
-    params.push(JSON.parse(req.query.isMarried))
-    queries.push(`isMarried = $${params.length}`)
+  if (req.query.ismarried) {
+    params.push(JSON.parse(req.query.ismarried))
+    queries.push(`ismarried = $${params.length}`)
   }
 
   let sql = 'SELECT COUNT(*) AS total FROM siswa';
@@ -74,9 +76,8 @@ router.get('/', (req, res) => {
     db.query(sql, params, (err, data) => {
       if (err) {
         console.log(err)
-
       }
-      res.render('users/table', { rows: data.rows, query: req.query, pages, page: parseInt(page), url: new URLSearchParams({ ...req.query, page }).toString() });
+      res.render('users/table', { moment, rows: data.rows, query: req.query, pages, page: parseInt(page), url: new URLSearchParams({ ...req.query, page }).toString() });
     })
   })
 });
@@ -85,38 +86,38 @@ router.get('/add', (req, res) => {
   res.render('users/form', { item: {} });
 })
 router.post('/add', (req, res) => {
-  const { name, height, weight, birthdate, isMarried } = req.body
-  db.run("INSERT INTO siswa (name, height, weight, birthdate, isMarried) VALUES (?, ?, ?, ?, ?)", [name, height, weight, birthdate, isMarried == "" ? null : JSON.parse(isMarried)], (err) => {
+  const { name, height, weight, birthdate, ismarried } = req.body
+  db.query("INSERT INTO siswa (name, height, weight, birthdate, ismarried) VALUES ($1, $2, $3, $4, $5)", [name, height, weight, birthdate, ismarried == "" ? null : JSON.parse(ismarried)], (err) => {
     if (err) {
       console.log("gagal menambah data", err)
     }
-    res.redirect('/')
+    res.redirect('/users')
   })
 })
 
 router.get('/edit/:id', (req, res) => {
   const id = req.params.id
-  db.get("SELECT * FROM siswa WHERE id = ?", [id], (err, item) => {
+  db.query("SELECT * FROM siswa WHERE id = $1", [id], (err, data) => {
     if (err) console.log(err)
-    res.render('users/form', { item })
+    res.render('users/form', { moment, item: data.rows[0] })
   })
 })
 
 router.post('/edit/:id', (req, res) => {
   const id = req.params.id
-  const { name, height, weight, birthdate, isMarried } = req.body
-  db.run("UPDATE siswa SET name = ?, height = ?, weight = ?, birthdate = ?, isMarried = ? WHERE id = ?",
-    [name, height, weight, birthdate, isMarried == "" ? null : JSON.parse(isMarried), id], (err) => {
+  const { name, height, weight, birthdate, ismarried } = req.body
+  db.query("UPDATE siswa SET name = $1, height = $2, weight = $3, birthdate = $4, ismarried = $5 WHERE id = $6",
+    [name, height, weight, birthdate, ismarried == "" ? null : JSON.parse(ismarried), id], (err) => {
       if (err) console.log("gagal update data", err)
-      res.redirect('/')
+      res.redirect('/users')
     })
 })
 
 router.get('/delete/:id', (req, res) => {
   const id = req.params.id
-  db.run('DELETE FROM siswa WHERE id = ?', [id], (err) => {
+  db.query('DELETE FROM siswa WHERE id = $1', [id], (err) => {
     if (err) console.log(err)
-    res.redirect('/')
+    res.redirect('/users')
   })
 })
 
@@ -131,17 +132,16 @@ router.post('/upload/:id', function(req, res) {
     return res.status(400).send('No files were uploaded.');
   }
 
-  // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
   let avatar = req.files.avatar;
   let filename = `${Date.now()}-${avatar.name}`;
-  let uploadPath = path.join(__dirname, 'public', 'avatars', filename); 
+  let uploadPath = path.join(__dirname, '..', 'public', 'images', 'avatars', filename); 
 
   console.log(uploadPath)
   avatar.mv(uploadPath, function(err) {
     if (err) return res.status(500).send(err);
-    db.run('UPDATE siswa SET avatar = ? WHERE id = ?', [filename, id], (err) => {
+    db.query('UPDATE siswa SET avatar = $1 WHERE id = $2', [filename, id], (err) => {
       if (err) console.log(err)
-        res.redirect('/');
+        res.redirect('/users');
     }) 
   });
 });
